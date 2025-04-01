278d16a85a96c684215e57bfa0175f6950b92c38
Thiago Sampaio <thiago.sampaiog@gmail.com>
2025-04-01 19:58:47 -0300

    chore: adding cors

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require("express");
+const cors = require("cors");
 const conn = require("./src/db/conn");
 require("./src/models");
 const carRoutes = require("./src/routes/carRoutes");
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(express.json());
 
 console.log("Rotas carregadas!");
